5741310a0b82d3990acce81ab9834405dd7cdcc2
SDANIS_kbcgroup <sdanis@csob.cz>
2024-09-25 18:23:22 +0200

    add private rest api

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -20,3 +20,26 @@ module "api_gateway_frontend" {
   # vpce_endpoint_ids = [data.aws_vpc_endpoint.selected.id]
   vpce_endpoint_ids = []
 }
+
+
+module "api_gateway_frontend_private" {
+  source = "./modules/aws-tf-module-api-gateway"
+
+  name      = "${local.name_prefix}-${var.project_name}-private-api"
+  pillar    = var.pillar
+  subsystem = var.subsystem
+
+  stage             = var.pillar
+  account_id        = var.aws_account_fe_num
+  api_region        = var.aws_region
+  description       = "API gateway for static frontend"
+  openapi_file_path = "openapi_definition/rrz-development-rag-frontend-api.yaml"
+  openapi_params = {
+    aws_account_num = var.aws_account_fe_num
+    aws_region = var.aws_region
+    aws_role = "APIGatewayS3ProxyPolicy"
+    aws_s3_bucket = module.s3_bucket_frontend.bucket_name
+  }
+  disable_execute_api_endpoint = false
+  vpce_endpoint_ids = [aws_vpc_endpoint.vpc_endpoint.id]
+}
--- terraform/data_vpc_endpoint.tf
+++ terraform/data_vpc_endpoint.tf
@@ -9,3 +9,48 @@
 #     values = ["com.amazonaws.${var.aws_region}.execute-api"]
 #   }
 # }
+
+data "aws_vpc" "default" {
+  tags = {
+    subsystemcode = upper(var.subsystem)
+  }
+}
+
+resource "aws_security_group" "api_gw_sg" {
+  name        = "api-gw-vpce-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  egress {
+    from_port        = 80
+    to_port          = 8443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 8443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  timeouts {
+    delete = "45m" // reason details https://registry.terraform.io/providers/hashicorp/aws/2.58.0/docs/resources/security_group
+  }
+}
+
+resource "aws_vpc_endpoint" "vpc_endpoint" {
+  private_dns_enabled = true
+  security_group_ids  = [aws_security_group.api_gw_sg.id]
+  service_name        = "com.amazonaws.${var.aws_region}.execute-api"
+  subnet_ids          = data.aws_subnets.selected.ids
+  vpc_endpoint_type   = "Interface"
+  vpc_id              = data.aws_vpc.default.id
+}
